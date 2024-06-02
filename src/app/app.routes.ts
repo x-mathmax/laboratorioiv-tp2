ada20fdec1f35d81f5afc832a37ea866af99a33c
x-mathmax <barbaramiamolinari@gmail.com>
2024-06-02 04:42:58 -0300

    avances visuales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/welcome', pathMatch: "full" },
+    { path: 'welcome',
+    loadComponent: () => import('./components/welcome/welcome.component').then(c => c.WelcomeComponent) },
+    { path: 'login',
+    loadComponent: () => import('./components/login/login.component').then(c => c.LoginComponent) },
+    { path: 'registro/especialista',
+    loadComponent: () => import('./components/alta-especialistas/alta-especialistas.component').then(c => c.AltaEspecialistasComponent) },
+];
+
+// export const routes: Routes = [
+//     { path: '', redirectTo: '/login', pathMatch: "full" },
+//     { path: 'login',
+//     loadComponent: () => import('./components/login/login.component').then(c => c.LoginComponent) },
+//     { path: 'home',
+//     loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent) },
+//     { path: 'about',
+//     loadComponent: () => import('./components/about-me/about-me.component').then(c => c.AboutMeComponent) },
+//     { path: 'register',
+//     loadComponent: () => import('./components/register/register.component').then(c => c.RegisterComponent) },
+//     { path: 'chatroom',
+//     loadComponent: () => import('./components/chatroom/chatroom.component').then(c => c.ChatroomComponent) },
+//     { path: 'hangman',
+//     loadComponent: () => import('./components/hangman/hangman.component').then(c => c.HangmanComponent) },
+//     { path: 'roulette',
+//     loadComponent: () => import('./components/roulette/roulette.component').then(c => c.RouletteComponent) },
+//     { path: 'greaterorlesser',
+//     loadComponent: () => import('./components/greaterorlesser/greaterorlesser.component').then(c => c.GreaterorlesserComponent) },
+//     { path: 'quizgame',
+//     loadComponent: () => import('./components/quizgame/quizgame.component').then(c => c.QuizgameComponent) },
+//     { path: '**',
+//     loadComponent: () => import('./components/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent) },
+//     //{ path: '**', component: PageNotFoundComponent }
+// ];
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
+
+  constructor(private router: Router){}
+  
+  goLogin():void {
+    this.router.navigate(['/login']);
+  }
 
 }
