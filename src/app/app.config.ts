f733e49b9d3ce993c19123c2874dc8a1bd1ca23a
x-mathmax <barbaramiamolinari@gmail.com>
2024-06-01 21:53:35 -0300

    base funcional

---
+++ src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { ApplicationConfig,  importProvidersFrom  } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { enviromentConfig } from '../environmentConfig';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(enviromentConfig))), 
+    importProvidersFrom(provideAuth(() => getAuth())), 
+    importProvidersFrom(provideFirestore(() => getFirestore())), 
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    provideHttpClient()
+  ],
+};
---
+++ src/environmentConfig.ts
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+export const enviromentConfig = {
+    apiKey: "AIzaSyAtaQLqhnXpDx3dJPok4TUGFHD4XS7WG8g",
+    authDomain: "laboratorioiv-tp2.firebaseapp.com",
+    projectId: "laboratorioiv-tp2",
+    storageBucket: "laboratorioiv-tp2.appspot.com",
+    messagingSenderId: "192654950846",
+    appId: "1:192654950846:web:a39aa27c004652625039ef"
+};
+// Initialize Firebase
+// const app = initializeApp(enviromentConfig);
